aee26ea311b124799ca703d50e184cefb0909bb6
Enrique0321 <Enriqueluli193@gmail.com>
2025-09-02 15:53:11 -0600

    feat(login):add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,14 +9,70 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool isPasswordVisible = false;
   @override
   Widget build(BuildContext context) {
+    //Para obtener el tamaño de pantalla de dispositivo
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Column(
+          //Axis o eje vertical
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Expanded(
+            SizedBox(
+              //Ancho de la pantalla calculado por MQ
+              width: size.width,
+              height: 200,
+
               child: RiveAnimation.asset('animated_login_character.riv'),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Email',
+                prefixIcon: const Icon(Icons.mail),
+                border: OutlineInputBorder(
+                  //Bordes ciruclares
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+
+            TextField(
+              //Para que se oculte la contraseña
+              obscureText: !isPasswordVisible,
+
+              ///Nueva propiedad
+              decoration: InputDecoration(
+                hintText: "Password",
+                prefixIcon: const Icon(Icons.lock),
+                //Nuevo icono para mostrar u ocultar la contraseña
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      isPasswordVisible = !isPasswordVisible;
+                    });
+                  },
+                ),
+                border: OutlineInputBorder(
+                  //Bordes ciruclares
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: size.width,
+              child: const Text(
+                "Forgot your Password?",
+                textAlign: TextAlign.right,
+                style: TextStyle(decoration: TextDecoration.underline),
+              ),
             ),
           ],
         ),
